894e8eab5c603f4bad758019519c1d50798079a4
miao <miao@miaodeMacBook-Pro.local>
2022-10-16 23:45:38 +0800

    version 1

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 rm -f /READ*
 rm -rf node_modules
-npm config set fetch-retry-mintimeout 20000
-npm config set fetch-retry-maxtimeout 120000
+# npm config set fetch-retry-mintimeout 20000
+# npm config set fetch-retry-maxtimeout 120000
 npm config ls -l 
 # npm install --registry=https://registry.npmmirror.com
 npm install --registry=https://registry.npm.taobao.org
