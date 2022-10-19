981e2f1a52a5a2937d2f88ea0e84d9a2c966d6b7
Your Name <you@example.com>
2022-10-19 16:38:07 +0800

    version 1

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export GOOS=linux
 export GOARCH=amd64
 version=${1:-latest}
 env=$2
-docker build --no-cache -t ccr.ccs.tencentyun.com/yorha-kf-demo/slug:${version} -f Dockerfile-${env} . 
-docker push ccr.ccs.tencentyun.com/yorha-kf-demo/slug:${version}
+docker build --no-cache -t marklinglon/slug:${version} -f Dockerfile-${env} . 
+docker push marklinglon/yorha-kf-demo/slug:${version}
