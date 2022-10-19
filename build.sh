936fd611b5ea9c9a8faa09cc6493cb618b05b1b4
Your Name <you@example.com>
2022-10-19 16:45:18 +0800

    version 1

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ export GOARCH=amd64
 version=${1:-latest}
 env=$2
 docker build --no-cache -t marklinglon/slug:${version} -f Dockerfile-${env} . 
-docker push marklinglon/yorha-kf-demo/slug:${version}
+docker push marklinglon/slug:${version}
